10ebce6b6221e5b83d8353a53aba786c58c71370
Goudmant <yvangoudmant@gmail.com>
2021-04-14 12:11:18 +0200

    v2

---
+++ scripttest.js
@@ -0,0 +1,95 @@
+class Question {
+    constructor(text, choices, answer) {
+      this.text = text;
+      this.choices = choices;
+      this.answer = answer;
+    }
+    isCorrectAnswer(choice) {
+      return this.answer === choice;
+    }
+  }
+  let questions = [
+    new Question("Réalisez-vous des démarches en ligne seul(e) ?", ["Toujours", "Jamais", "Rarement", "Souvent"], "Toujours"),
+    new Question("Utilisez-vous régulièrement votre adresse email pour communiquer ?", ["Toujours", "Jamais", "Rarement", "Souvent"], "Toujours"),
+    new Question("Utilisez-vous internet pour chercher des informations ?", ["Toujours", "Jamais", "Rarement", "Souvent"], "Toujours"),
+    new Question("Utilisez vous professionnellement une smartphone ou une tablette ?", ["Toujours", "Jamais", "Rarement", "Souvent"], "Toujours"),
+  ];
+  
+  console.log(questions);
+  
+  class Quiz {
+    constructor(questions) {
+      this.score = 0;
+      this.questions = questions;
+      this.currentQuestionIndex = 0;
+    }
+    getCurrentQuestion() {
+      return this.questions[this.currentQuestionIndex];
+    }
+    guess(answer) {
+      if (this.getCurrentQuestion().isCorrectAnswer(answer)) {
+        this.score++;
+      }
+      this.currentQuestionIndex++;
+    }
+    hasEnded() {
+      return this.currentQuestionIndex >= this.questions.length;
+    }
+  }
+  
+  // Regroup all  functions relative to the App Display
+  const display = {
+    elementShown: function(id, text) {
+      let element = document.getElementById(id);
+      element.innerHTML = text;
+    },
+    endQuiz: function() {
+      endQuizHTML = `
+        <h1>Questionnaire terminé !</h1>
+        <h3> Votre score est de : ${quiz.score} / ${quiz.questions.length}</h3>`;
+      this.elementShown("quiz", endQuizHTML);
+    },
+    question: function() {
+      this.elementShown("question", quiz.getCurrentQuestion().text);
+    },
+    choices: function() {
+      let choices = quiz.getCurrentQuestion().choices;
+  
+      guessHandler = (id, guess) => {
+        document.getElementById(id).onclick = function() {
+          quiz.guess(guess);
+          quizApp();
+        }
+      }
+      // display choices and handle guess
+      for(let i = 0; i < choices.length; i++) {
+        this.elementShown("choice" + i, choices[i]);
+        guessHandler("guess" + i, choices[i]);
+      }
+    },
+    progress: function() {
+      let currentQuestionNumber = quiz.currentQuestionIndex + 1;
+      this.elementShown("progress", "Question " + currentQuestionNumber + " sur " + quiz.questions.length);
+    },
+  };
+  
+  // Game logic
+  quizApp = () => {
+    if (quiz.hasEnded()) {
+      display.endQuiz();
+    } else {
+      display.question();
+      display.choices();
+      display.progress();
+    } 
+  }
+  // Create Quiz
+  let quiz = new Quiz(questions);
+  quizApp();
+  
+  // Result
+
+  
+  console.log(quiz);
+  
+  
